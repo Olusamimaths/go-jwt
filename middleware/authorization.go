6bf37e5c96df7b9d3ba04a814bc87b6b0b668ca7
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-01-24 21:41:08 +0100

    🔥 MiddleWare

---
+++ middleware/authorization.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/olusamimaths/go-jwt/model"
+)
+
+func Authorize(c *gin.Context) {
+	tokenString, err := c.Cookie("Authorization")
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["sub"])
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		var user model.User
+		model.DB.Where("email =?", claims["sub"]).First(&user)
+		if user.Email == "" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		c.Set("user", user)
+		c.Next()
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+}
